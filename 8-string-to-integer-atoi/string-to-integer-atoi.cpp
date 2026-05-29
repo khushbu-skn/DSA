3675192690afe0726f7993d5d86d0e4e70ee2444
khushbu-skn <skn14635718@gmail.com>
2026-05-29 11:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (20.49%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
+    int helper(const string &s, int i, long long num, int sign) {
+        if (i >= s.size() || !isdigit(s[i]))
+            return sign * num;
+
+        num = num * 10 + (s[i] - '0');
+
+        if (sign == 1 && num > INT_MAX)
+            return INT_MAX;
+
+        if (sign == -1 && -num < INT_MIN)
+            return INT_MIN;
+
+        return helper(s, i + 1, num, sign);
+    }
+
     int myAtoi(string s) {
-        long long ans=0;
-        int i=0;
-        int n=s.length();
-        int sign=1;
-        while(i<n && s[i]==' ') i++;
-        if(i<n && s[i]=='+' || s[i]=='-'){
-            if(s[i]=='-') sign=-1;
+        int i = 0;
+
+        while (i < s.size() && s[i] == ' ')
+            i++;
+
+        int sign = 1;
+        if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
             i++;
         }
-        while(i<n && s[i]=='0') i++;
-        while(i<n && isdigit(s[i])){
-           int digit = s[i] - '0';
 
-            if(ans > (INT_MAX - digit) / 10) {
-
-                if(sign == 1)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
-            ans=(ans*10LL)+(s[i]-'0');
-            i++;
-        }
-        ans*=sign;
-        if (ans<INT_MIN) return INT_MIN;
-        if(ans>INT_MAX) return INT_MAX;
-        return ans; 
+        return helper(s, i, 0, sign);
     }
 };
